d85e5281547ccaced9fa93a3d9dd2faf5d2613f4
jinyue Jiang <jinyue.jiang.0115@gmail.com>
2020-01-20 00:09:20 -0500

    Create 215. Kth Largest Element in an Array.java

---
+++ Div&Con/215. Kth Largest Element in an Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int max = Integer.MIN_VALUE;
+        for (int i : nums) {
+            if (i > max) max = i;
+            if (map.get(i) == null) map.put(i, 1);
+            else map.replace(i, map.get(i) + 1);
+        }
+        int sum = 0;
+        while (true) {
+            if (map.get(max) == null) {
+                max--;
+                continue;
+            }
+            sum += map.get(max);
+            if (sum >= k) return max;
+            else max -= 1;
+        }
+    }
+}
+
+its runtime is depend on the range, still need development.
